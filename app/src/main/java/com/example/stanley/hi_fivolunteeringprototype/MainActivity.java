8ca09cb2c344f3a4de668a0ac17e2f903e9e153c
stanleykelder <stanleykelder@gmail.com>
2019-05-03 17:36:43 +0200

    first creation of the homepage screen

--- app/src/main/java/com/example/stanley/hi_fivolunteeringprototype/MainActivity.java
+++ app/src/main/java/com/example/stanley/hi_fivolunteeringprototype/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.stanley.hi_fivolunteeringprototype;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.menu_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.menu_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                        case R.id.menu_new_event:
+                            selectedFragment = new NotImplementedFragment();
+                            break;
+
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
