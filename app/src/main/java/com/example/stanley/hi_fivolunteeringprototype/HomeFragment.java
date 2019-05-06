d4762e45f94216aa1d379a33c16fda50f34a88ff
stanleykelder <stanleykelder@gmail.com>
2019-05-06 23:10:02 +0200

    bottom navigation working properly. Button and helps redirecting

--- app/src/main/java/com/example/stanley/hi_fivolunteeringprototype/HomeFragment.java
+++ app/src/main/java/com/example/stanley/hi_fivolunteeringprototype/HomeFragment.java
@@ -1,11 +1,15 @@
 package com.example.stanley.hi_fivolunteeringprototype;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.support.design.widget.BottomNavigationView;
+import android.widget.Toast;
 
 
 public class HomeFragment extends Fragment {
@@ -14,5 +18,30 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home_constraint, container, false);
-    }
+        }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        final Context context = getActivity().getApplicationContext();
+
+        ImageView button = (ImageView) getView().findViewById(R.id.find_where_to_help);
+        // or  (ImageView) view.findViewById(R.id.foo);
+            button.setOnClickListener(new View.OnClickListener() {
+            Fragment selectedFragment = new SearchFragment();
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+            }
+        });
+
+        ImageView helps = (ImageView) getView().findViewById(R.id.helps_image);
+        // or  (ImageView) view.findViewById(R.id.foo);
+        helps.setOnClickListener(new View.OnClickListener() {
+            Fragment selectedFragment = new HelpDetailsFragment();
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+            }
+        });
+    };
 }
--- app/src/main/java/com/example/stanley/hi_fivolunteeringprototype/MainActivity.java
+++ app/src/main/java/com/example/stanley/hi_fivolunteeringprototype/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.stanley.hi_fivolunteeringprototype;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+        bottomNav.getMenu().setGroupCheckable(0, false, true);
 
         //I added this if statement to keep the selected fragment when rotating the device
         if (savedInstanceState == null) {
