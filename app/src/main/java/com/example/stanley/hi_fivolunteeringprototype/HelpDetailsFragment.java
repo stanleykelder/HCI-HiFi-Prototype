1b1b9d19341c3ec2cbb709b43ea13417d045cec6
Jie <jie.gao1995@gmail.com>
2019-05-09 09:29:09 +0200

    Revert "back navigation with arrow in toolbar and harware arrow. Change of title in toolbar"
    This reverts commit 209e40eafe0f84b566289c09713da603a85ea9d7.

--- app/src/main/java/com/example/stanley/hi_fivolunteeringprototype/HelpDetailsFragment.java
+++ app/src/main/java/com/example/stanley/hi_fivolunteeringprototype/HelpDetailsFragment.java
@@ -8,11 +8,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-public class SearchFragment extends Fragment {
+public class HelpDetailsFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        return inflater.inflate(R.layout.help_details_fragment, container, false);
     }
 }
--- app/src/main/java/com/example/stanley/hi_fivolunteeringprototype/MainActivity.java
+++ app/src/main/java/com/example/stanley/hi_fivolunteeringprototype/MainActivity.java
@@ -1,34 +1,27 @@
 package com.example.stanley.hi_fivolunteeringprototype;
 
-import android.os.Build;
+import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-
-import java.util.Stack;
-
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-        Stack<Fragment> stack = new Stack<Fragment>();
-
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+        bottomNav.getMenu().setGroupCheckable(0, false, true);
 
         //I added this if statement to keep the selected fragment when rotating the device
         if (savedInstanceState == null) {
@@ -36,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
                     new HomeFragment()).commit();
         }
     }
-
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -57,35 +49,10 @@ public class MainActivity extends AppCompatActivity {
 
                     }
 
-                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                    fragmentTransaction.replace(R.id.fragment_container, selectedFragment);
-                    fragmentTransaction.commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
 
                     return true;
                 }
             };
-
-
-    public void setFragmentTitle(String title){
-        Toolbar toolbar = findViewById(R.id.my_toolbar);
-        toolbar.setTitle(title);
-    }
-
-    public void addArrow(){
-        Toolbar toolbar = findViewById(R.id.my_toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
-
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
-    }
-
-    public void removeArrow(){
-        Toolbar toolbar = findViewById(R.id.my_toolbar);
-        toolbar.setNavigationIcon(null);
-    }
-
 }
