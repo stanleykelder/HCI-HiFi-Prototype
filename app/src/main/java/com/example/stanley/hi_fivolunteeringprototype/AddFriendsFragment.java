ecdf4e65d5d7afe8af6962ed91d2f067090935cb
Lukas Bittner <4bittner@informatik.uni-hamburg.de>
2019-05-14 17:13:52 +0200

    Add friends fregment added

---
+++ app/src/main/java/com/example/stanley/hi_fivolunteeringprototype/AddFriendsFragment.java
@@ -0,0 +1,26 @@
+package com.example.stanley.hi_fivolunteeringprototype;
+
+import android.animation.ObjectAnimator;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+public class AddFriendsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_friends, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        ((MainActivity) getActivity()).setTitle("Add Friends");
+        ((MainActivity) getActivity()).addArrow();
+//        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+    }
+}
